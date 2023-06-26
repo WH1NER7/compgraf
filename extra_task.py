fcfe023db5822e367bb92b997fc574c0b405e2d4
WH1NER7 <danil-2208@mail.ru>
2023-06-26 22:25:59 +0500

    extra task

--- extra_task.py
+++ extra_task.py
@@ -1,58 +1,66 @@
-import sys
-import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLUT import *
 from PIL import Image
-import OpenGL.GL as gl
-import OpenGL.GLUT as glut
 
-# Загрузка файла с символами "0" и "1"
-file_path = "input.txt"
-with open(file_path, "r") as file:
-    lines = file.readlines()
 
-# Определение размеров изображения
-image_height = len(lines)
-image_width = len(lines[0].strip())
+def load_image_data(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
 
-# Создание пустого изображения
-image_data = np.zeros((image_height, image_width, 3), dtype=np.uint8)
+    # Удаляем символы перевода строки
+    lines = [line.rstrip('\r\n') for line in lines]
 
-# Преобразование символов в цвета пикселей
-for y, line in enumerate(lines):
-    line = line.strip()
-    for x, char in enumerate(line):
-        if char == "0":
-            image_data[y, x] = [0, 0, 0]  # Черный цвет для "0"
-        elif char == "1":
-            image_data[y, x] = [255, 255, 255]  # Белый цвет для "1"
+    # Преобразование строк в список байтовых значений
+    image_data = [int(char) for line in lines for char in line]
+    # print(image_data)
+    return image_data
 
-# Создание объекта Image из данных изображения
-image = Image.fromarray(image_data)
 
-# Сохранение изображения в формате BMP
-output_path = "output.bmp"
-image.save(output_path)
+def create_bitmap_image(image_data, width, height):
+    # Создание пустого изображения
+    image = Image.new('RGB', (width, height), 'white')
+    pixels = image.load()
 
-# Функция отображения
+    # Установка цвета пикселей в соответствии с данными изображения
+    for y in range(height):
+        for x in range(width):
+            pixel_value = image_data[y * width + x]
+            color = (0, 0, 0) if pixel_value == 1 else (255, 255, 255)
+            pixels[x, y] = color
+
+    return image
+
+
 def display():
-    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-    gl.glRasterPos2i(-1, -1)
-    gl.glDrawPixels(image_width, image_height, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, image_data)
-    glut.glutSwapBuffers()
+    glClear(GL_COLOR_BUFFER_BIT)
 
-# Функция изменения размеров окна
-def reshape(width, height):
-    gl.glViewport(0, 0, width, height)
+    # Отображение изображения с использованием glDrawPixels
+    glRasterPos2f(-1, -1)
+    glDrawPixels(image.width, image.height, GL_RGB, GL_UNSIGNED_BYTE, image.tobytes())
+
+    glFlush()
 
 
-# Инициализация GLUT
-glut.glutInit(sys.argv)
-glut.glutInitDisplayMode(glut.GLUT_RGB | glut.GLUT_DOUBLE | glut.GLUT_DEPTH)
-glut.glutInitWindowSize(image_width, image_height)
-glut.glutCreateWindow("Image Viewer")
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(image.width, image.height)
+    glutCreateWindow(b"PyOpenGL Example")
 
-# Задание функций отображения и изменения размеров окна
-glut.glutDisplayFunc(display)
-glut.glutReshapeFunc(reshape)
+    glutDisplayFunc(display)
 
-# Запуск основного цикла GLUT
-glut.glutMainLoop()
+    glutMainLoop()
+
+
+if __name__ == "__main__":
+    # Загрузка данных из файла
+    image_data = load_image_data("input.txt")
+
+    # Вычисление ширины и высоты изображения
+    width = 255
+    height = len(image_data) // width
+
+    # Создание битмап-изображения
+    image = create_bitmap_image(image_data, width, height)
+
+    main()
